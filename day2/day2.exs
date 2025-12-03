a3f2120c5de33855c2b58f53352749568e23a2cc
Venkatesh-Prasad Ranganath <rvprasad@users.noreply.github.com>
2025-12-02 19:01:23 -0600

    Solve day 2 puzzle

--- day2/day2.exs
+++ day2/day2.exs
@@ -1,32 +1,48 @@
 defmodule Day2 do
-  require Integer
-
-  def read_id_ranges(filename) do
+  def read_ranges(filename) do
     File.stream!(filename)
     |> Enum.to_list()
     |> Enum.map(&String.trim_trailing/1)
     |> hd()
     |> String.split(",")
     |> Enum.map(fn l ->
-      String.split(l, "-") |> Enum.map(&String.to_integer/1)
+      String.split(l, "-")
     end)
   end
 
-  def find_invalid_ids([l, h]) do
-    Enum.to_list(l..h)
+  def find_invalid_ids([l, h], is_invalid) do
+    Enum.to_list(String.to_integer(l)..String.to_integer(h))
     |> Enum.map(&Integer.to_string/1)
-    |> Enum.filter(&Integer.is_even(String.length(&1)))
-    |> Enum.filter(fn s ->
-      {a, b} = String.split_at(s, div(String.length(s), 2))
-      a == b
-    end)
+    |> Enum.filter(is_invalid)
     |> Enum.map(&String.to_integer/1)
   end
 
-  def solve_part_1(id_ranges) do
-    id_ranges |> Enum.flat_map(&Day2.find_invalid_ids/1) |> Enum.sum()
+  def is_invalid_1(s) do
+    {a, b} = String.split_at(s, div(String.length(s), 2))
+    a == b
+  end
+
+  def is_invalid_2(s) do
+    s_len = String.length(s)
+
+    s_len > 1 &&
+      Enum.to_list(1..div(s_len, 2))
+      |> Enum.any?(fn l ->
+        String.graphemes(s)
+        |> Enum.chunk_every(l)
+        |> MapSet.new()
+        |> MapSet.size() == 1
+      end)
+  end
+
+  def solve_puzzle(id_ranges, is_invalid) do
+    id_ranges
+    |> Enum.flat_map(&Day2.find_invalid_ids(&1, is_invalid))
+    |> Enum.sum()
   end
 end
 
 filename = System.argv() |> List.first()
-Day2.read_id_ranges(filename) |> Day2.solve_part_1() |> IO.puts()
+ranges = Day2.read_ranges(filename)
+ranges |> Day2.solve_puzzle(&Day2.is_invalid_1/1) |> IO.puts()
+ranges |> Day2.solve_puzzle(&Day2.is_invalid_2/1) |> IO.puts()
---
+++ day2/src/main.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if let Ok(ranges) = read_ranges(&args[1]) {
+        println!("{}", solve_puzzle(&ranges, &is_invalid_1));
+        println!("{}", solve_puzzle(&ranges, &is_invalid_2));
+    }
+}
+fn read_ranges(filename: &String) -> std::io::Result<Vec<(String, String)>> {
+    let f = File::open(filename)?;
+    Ok(BufReader::new(f)
+        .lines()
+        .flatten()
+        .filter(|l| !l.is_empty())
+        .map(|l| {
+            l.split(',')
+                .map(|s| {
+                    let tmp1: Vec<&str> = s.split('-').collect();
+                    (tmp1[0].to_string(), tmp1[1].to_string())
+                })
+                .collect::<Vec<(String, String)>>()
+        })
+        .flatten()
+        .collect())
+}
+
+type IsInvalid = dyn Fn(String) -> bool;
+
+fn solve_puzzle(ranges: &Vec<(String, String)>, is_invalid: &IsInvalid) -> i64 {
+    ranges
+        .iter()
+        .flat_map(|(l, h)| {
+            (l.parse::<i64>().unwrap()..(h.parse::<i64>().unwrap() + 1))
+                .filter(|i| is_invalid(i.to_string()))
+        })
+        .sum()
+}
+
+fn is_invalid_1(s: String) -> bool {
+    let (a, b) = s.split_at(s.len() / 2);
+    a == b
+}
+
+fn is_invalid_2(s: String) -> bool {
+    let s_len = s.len();
+    let s_chars = s.chars().collect::<Vec<char>>();
+
+    s_len > 1
+        && (1..(s_len / 2 + 1)).any(|l| s_chars.chunks(l).collect::<HashSet<&[char]>>().len() == 1)
+}
